6cd0eff7df833253c72a48690d1fc13adad3b58b
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-09-20 15:59:18 +0100

    Calcular o carro

--- ManipulaNumerosR/src/manipulanumerosr/Carro.java
+++ ManipulaNumerosR/src/manipulanumerosr/Carro.java
@@ -1,9 +1,27 @@
 
 package manipulanumerosr;
 
+import static manipulanumerosr.ManipulaNumerosR.ler;
+
 class Carro {
 
     static void calculaValorFinal() {
+        float i;
+        float d;
+        int op;
+        
+        float precoD;
+        float precoI;
+        System.out.println("Digite o preço do carro: ");
+        op = ler.nextInt();
+        d = (float) 0.28;
+        i = (float) 0.45;
+        
+        precoD = op * d;
+        precoI = op * i;
+        float precoF = op - (precoD + precoI);
+        System.out.println("O custo de fabrica do carro é " + precoF);
     }
     
 }
+
